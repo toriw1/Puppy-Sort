a2541072883c1a3e919e8ed0def2bc7cbc0a29ea
Tori Williams <vwilliams@guilford.edu>
2023-04-28 18:21:14 -0400

    Puppy Sort Project

---
+++ src/main/java/edu/guilford/Puppy.java
@@ -0,0 +1,201 @@
+package edu.guilford;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class Puppy {
+
+    // attributes
+    private String name;
+    private int age;
+    private String breed;
+    private String color;
+    private String furtype;
+    private String size;
+    private double weight;
+    private boolean spots;
+    private String sex;
+
+    Random rand = new Random();
+
+    // constructor
+    public Puppy(String name, String size, double weight, int age, String breed, String color, String furtype,
+            boolean spots, String sex) {
+        this.name = name;
+        this.weight = weight;
+        this.spots = spots;
+        this.age = age;
+        this.breed = breed;
+        this.color = color;
+        this.furtype = furtype;
+        this.size = size;
+        this.sex = sex;
+    }
+
+    // random constructor that sets the attributes to random values
+    public Puppy() {
+        // choose a random puppy name from a list of 20
+        String[] puppyNames = { "Bella", "Charlie", "Luna", "Lucy", "Max", "Sissy", "Cooper", "Daisy", "Sadie",
+                "Molly", "Buddy", "Lola", "Stella", "Tinker", "Bear", "Stevie", "Duke", "Penny", "Sophie", "Harley" };
+        int randomName = rand.nextInt(puppyNames.length);
+        this.name = puppyNames[randomName];
+        // choose a random size from a list of 3
+        String[] puppySizes = { "Small", "Medium", "Large" };
+        int randomSize = rand.nextInt(puppySizes.length);
+        this.size = puppySizes[randomSize];
+        // choose a random weight between 1.0 and 100.0 based on the size
+        if (this.size == "Small") {
+            this.weight = rand.nextDouble() * 15;
+        } else if (this.size == "Medium") {
+            this.weight = rand.nextDouble() * 50 + 10;
+        } else {
+            this.weight = rand.nextDouble() * 50 + 50;
+        }
+        // choose a random age between 0 and 15
+        this.age = rand.nextInt(16);
+        // choose a random breed from a list of 10
+        String[] puppyBreeds = { "Labrador Retriever", "German Shepherd", "Golden Retriever", "Jack Russell",
+                "Bulldog", "Poodle", "Beagle", "Rottweiler", "Chihuahua", "Yorkie" };
+        int randomBreed = rand.nextInt(puppyBreeds.length);
+        this.breed = puppyBreeds[randomBreed];
+        // choose a random color from a list of 5
+        String[] puppyColors = { "Black", "White", "Brown", "Red", "Yellow" };
+        int randomColor = rand.nextInt(puppyColors.length);
+        this.color = puppyColors[randomColor];
+        // choose a random fur type from a list of 2
+        String[] puppyFurTypes = { "Short", "Long" };
+        int randomFurType = rand.nextInt(puppyFurTypes.length);
+        this.furtype = puppyFurTypes[randomFurType];
+        // choose a random boolean for spots based on a list of three options
+        String[] puppySpots = { "No Spots", "Few Spots", "Many Spots" };
+        int randomSpots = rand.nextInt(puppySpots.length);
+        if (puppySpots[randomSpots] == "No Spots") {
+            this.spots = false;
+        } else {
+            this.spots = true;
+        }
+        // choose a random sex based on a list of two options
+        String[] puppySex = { "Female", "Male" };
+        int randomSex = rand.nextInt(puppySex.length);
+        this.sex = puppySex[randomSex];
+    }
+
+    // constructor that just has a weight
+    public Puppy(double weight) {
+        this.weight = weight;
+    }
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getFurtype() {
+        return furtype;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public boolean isSpots() {
+        return spots;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFurtype(String furtype) {
+        this.furtype = furtype;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setSpots(boolean spots) {
+        this.spots = spots;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    // methods
+    public void bark() {
+        System.out.println("Woof!");
+    }
+
+    public void eat() {
+        System.out.println("Yum!");
+    }
+
+    public void cuddle() {
+        System.out.println("Pet me!");
+    }
+
+    public void play() {
+        System.out.println("Let's play!");
+    }
+
+    // write a method to help analyze weights of puppies
+    public void weightAnalysis() {
+        if (weight < 10) {
+            System.out.println("This puppy is very small!");
+        } else if (weight >= 10 && weight < 20) {
+            System.out.println("This puppy is small!");
+        } else if (weight >= 20 && weight < 40) {
+            System.out.println("This puppy is medium!");
+        } else if (weight >= 40 && weight < 60) {
+            System.out.println("This puppy is large!");
+        } else if (weight >= 60) {
+            System.out.println("This puppy is very large!");
+        }
+    }
+
+    // toString method
+    public String toString() {
+        DecimalFormat formatter = new DecimalFormat("#0.00");
+        return "Name: " + name + ", Size: " + size + ", Weight: " + formatter.format(weight) + ", Age: " + age
+                + ", Breed: " + breed + ", Color: " + color + ", Fur Type: " + furtype + ", Spots: " + spots + ", Sex: "
+                + sex;
+    }
+}
---
+++ src/main/java/edu/guilford/PuppySort.java
@@ -0,0 +1,211 @@
+package edu.guilford;
+
+import java.text.DecimalFormat;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PuppySort {
+    public static void main(String[] args) {
+        // instantiate a scanner object
+        Scanner scan = new Scanner(System.in);
+        // start time variable
+        long startTime;
+        // end time variable
+        long endTime;
+        // duration variable
+        long duration;
+        // decimal formatter
+        DecimalFormat smallFormatter = new DecimalFormat("#.####");
+
+        // instantiate an array of Puppy objects based on user input
+        System.out.println("How many puppies would you like to create?");
+        int numPuppies = scan.nextInt();
+        Puppy[] puppies = new Puppy[numPuppies];
+        for (int i = 0; i < numPuppies; i++) {
+            puppies[i] = new Puppy();
+        }
+        // // print the array of puppies
+        // System.out.println("\nHere are the puppies you created:");
+        // for (int i = 0; i < numPuppies; i++) {
+        //     System.out.println(puppies[i]);
+        // }
+
+        // sort the array of puppies using the selection sort algorithm and time it
+        startTime = System.nanoTime();
+        selectionSort(puppies);
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        // print the duration of the sort
+        System.out.println("\nSelectionSort took " + smallFormatter.format(duration / 1.e6)
+                + " milliseconds");
+        // System.out.println("\nHere are the puppies sorted by weight using selection sort:");
+        // for (int i = 0; i < numPuppies; i++) {
+        //     System.out.println(puppies[i]);
+        // }
+
+        // shuffle the array of puppies using the shuffle method
+        shuffle(puppies);
+
+        // sort the array of puppies using the quick sort algorithm and time it
+        startTime = System.nanoTime();
+        quickSort(puppies);
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        // print the duration of the sort
+        System.out.println("\nQuickSort took " + smallFormatter.format(duration / 1.e6) + " milliseconds");
+        // System.out.println("\nHere are the puppies sorted by weight using quick sort:");
+        // for (int i = 0; i < numPuppies; i++) {
+        //     System.out.println(puppies[i]);
+        // }
+
+        // shuffle the array of puppies using the shuffle method
+        shuffle(puppies);
+
+        // use the linear search algorithm to find a random puppy and time it
+        Puppy randomPuppy = puppies[(int) (Math.random() * numPuppies)];
+        startTime = System.nanoTime();
+        int linearIndex = linearSearch(puppies, randomPuppy);
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        // print the duration of the search
+        System.out.println("\nLinearSearch took " + smallFormatter.format(duration / 1.e6) + " milliseconds");
+        //System.out.println("\nHere is the puppy you searched for using linear search:");
+        //System.out.println(puppies[linearIndex]);
+
+        // use the binary search algorithm to find a random puppy and time it
+        // first, sort the array of puppies using the quick sort algorithm
+        quickSort(puppies);
+        // then, search for the random puppy
+        startTime = System.nanoTime();
+        int binaryIndex = binarySearch(puppies, randomPuppy);
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+        // print the duration of the search
+        System.out.println("\nBinarySearch took " + smallFormatter.format(duration / 1.e6) + " milliseconds");
+        //System.out.println("\nHere is the puppy you searched for using binary search:");
+        //System.out.println(puppies[binaryIndex]);
+    }
+
+    // add a static selectionSort method that takes an array of Puppy objects as a
+    // parameter and sorts them by weight using the selection sort algorithm
+    public static void selectionSort(Puppy[] puppies) {
+        // loop through the array
+        for (int i = 0; i < puppies.length - 1; i++) {
+            // set the minimum index to the current index
+            int minIndex = i;
+            // loop through the array again starting at the next index
+            for (int j = i + 1; j < puppies.length; j++) {
+                // if the current element is less than the element at the minimum index
+                if (puppies[j].getWeight() < puppies[minIndex].getWeight())
+                    // set the minimum index to the current index
+                    minIndex = j;
+            }
+            // call the swap method
+            swap(puppies, i, minIndex);
+        }
+    }
+
+    // add a static quickSort method that takes an array of Puppy objects as a
+    // parameter and sorts them by weight using the quick sort algorithm
+    public static void quickSort(Puppy[] puppies) {
+        // call the recursive quickSort method
+        quickSort(puppies, 0, puppies.length - 1);
+    }
+
+    // add a static recursive quickSort method that takes an array of Puppy objects
+    public static void quickSort(Puppy[] puppies, int left, int right) {
+        // if the left index is less than the right index
+        if (left < right) {
+            // partition the array
+            int pivotIndex = partition(puppies, left, right);
+            // recursively call quickSort on the left and right partitions
+            quickSort(puppies, left, pivotIndex - 1);
+            quickSort(puppies, pivotIndex + 1, right);
+        }
+    }
+
+    // add a static partition method that takes an array of Puppy objects
+    public static int partition(Puppy[] puppies, int left, int right) {
+        // set the pivot element to the rightmost element
+        Puppy pivot = puppies[right];
+        // set the partition index to the leftmost element
+        int partitionIndex = left;
+        // loop through the array
+        for (int i = left; i < right; i++) {
+            // if the current element is less than the pivot element
+            if (puppies[i].getWeight() < pivot.getWeight()) {
+                // swap the current element with the element at the partition index
+                swap(puppies, i, partitionIndex);
+                // increment the partition index
+                partitionIndex++;
+            }
+        }
+        // swap the pivot element with the element at the partition index
+        swap(puppies, right, partitionIndex);
+        // return the partition index
+        return partitionIndex;
+    }
+
+    // add a static linearSearch method that takes an array of Puppy objects
+    public static int linearSearch(Puppy[] puppies, Puppy puppy) {
+        // loop through the array
+        for (int i = 0; i < puppies.length; i++) {
+            // if the current element is equal to the Puppy object
+            if (puppies[i].equals(puppy)) {
+                // return the index of the Puppy object
+                return i;
+            }
+        }
+        // return -1
+        return -1;
+    }
+
+    // add a static binarySearch method that takes an array of Puppy objects
+    public static int binarySearch(Puppy[] puppies, Puppy puppy) {
+        // set the left index to 0
+        int left = 0;
+        // set the right index to the length of the array minus 1
+        int right = puppies.length - 1;
+        // loop while the left index is less than or equal to the right index
+        while (left <= right) {
+            // set the middle index to the average of the left and right indices
+            int middle = (left + right) / 2;
+            // if the Puppy object is equal to the element at the middle index
+            if (puppy.equals(puppies[middle])) {
+                // return the middle index
+                return middle;
+            }
+            // if the Puppy object is less than the element at the middle index
+            if (puppy.getWeight() < puppies[middle].getWeight()) {
+                // set the right index to the middle index minus 1
+                right = middle - 1;
+            }
+            // if the Puppy object is greater than the element at the middle index
+            if (puppy.getWeight() > puppies[middle].getWeight()) {
+                // set the left index to the middle index plus 1
+                left = middle + 1;
+            }
+        }
+        // return -1
+        return -1;
+    }
+
+    // add a static swap method that takes an array of Puppy objects and two indices
+    public static void swap(Puppy[] puppies, int i, int j) {
+        Puppy temp = puppies[i];
+        puppies[i] = puppies[j];
+        puppies[j] = temp;
+    }
+
+    // add a shuffle method that shuffles an array of Puppy objects
+    public static void shuffle(Puppy[] puppies) {
+        // loop through the array
+        for (int i = 0; i < puppies.length; i++) {
+            // generate a random index
+            int randomIndex = (int) (Math.random() * puppies.length);
+            // call the swap method
+            swap(puppies, i, randomIndex);
+        }
+    }
+}
